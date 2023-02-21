5f0b496fc542cf55da7e44766323c9a9bed75bda
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-02-21 15:55:42 +0900

    [Bronze I] Title: 딱지놀이, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); 
+		for (int tc = 1; tc <= N; tc++) {
+			String numA = br.readLine().replace(" ", "");
+			String numB = br.readLine().replace(" ", "");
+			
+			int starA = numA.length() - numA.replace("4", "").length();
+			int starB = numB.length() - numB.replace("4", "").length();
+			
+			int circleA = numA.length() - numA.replace("3", "").length();
+			int circleB = numB.length() - numB.replace("3", "").length();
+			
+			int squareA = numA.length() - numA.replace("2", "").length();
+			int squareB = numB.length() - numB.replace("2", "").length();
+			
+			int triA = numA.length() - numA.replace("1", "").length();
+			int triB = numB.length() - numB.replace("1", "").length();
+			// 별은 4 동그라미는 3 네모는 2 세모는 1
+			if (starA != starB) {
+				if (starA > starB) System.out.println("A");
+				else System.out.println("B");
+			} else if (starA == starB && circleA != circleB) {
+				if (circleA > circleB) System.out.println("A");
+				else System.out.println("B");
+			} else if (starA == starB && circleA == circleB && squareA != squareB) {
+				if (squareA > squareB) System.out.println("A");
+				else System.out.println("B");
+			} else if (starA == starB && circleA != circleB && squareA == squareB && triA != triB) {
+				if (triA > triB) System.out.println("A");
+				else System.out.println("B");
+			} else {
+				System.out.println("D");
+			}
+		}
+	}
+}
