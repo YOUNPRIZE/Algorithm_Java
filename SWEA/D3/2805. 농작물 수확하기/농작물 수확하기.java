5e05fa952a601ee6aa2256ed6f1ddfb8c655ed1f
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-02-24 15:04:22 +0900

    [D3] Title: 농작물 수확하기, Time: 191 ms, Memory: 29,668 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -14,29 +14,31 @@ public class Solution {
 					arr[i][j] = charArr[j] - '0';
 				}
 			}
+			// 색칠? 해야될 칸에 대한 변수 크기 설정
 			int blue = N / 2;
 			
+			// 총합 변수 선언 및 초기화
 			int sum = 0;
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (i == N / 2) {
+					// 행기준 중앙에 위치해있을 때
+//					if (i == N / 2) {
 						if (j >= blue && j < N - blue) {
 							sum += arr[i][j];
-							//System.out.println(arr[i][j]);
 						}
-					}
-					else if (i >= N - N / 2) {
-						if (j >= blue && j < N - blue) {
-							sum += arr[i][j];
-							//System.out.println(arr[i][j]);
-						}
-					} else {
-						if (j >= blue && j < N - blue) {
-							sum += arr[i][j];
-							//System.out.println(arr[i][j]);
-						}
-					}
+//					}
+//					// 행 기준
+//					else if (i >= N - N / 2) {
+//						if (j >= blue && j < N - blue) {
+//							sum += arr[i][j];
+//						}
+//					// 행기준 중앙보다 낮을 때
+//					} else {
+//						if (j >= blue && j < N - blue) {
+//							sum += arr[i][j];
+//						}
+//					}
 				}
 				if (i >= N / 2) blue++;
 				else blue--;
