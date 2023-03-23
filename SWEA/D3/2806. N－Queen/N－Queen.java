b744e100b4aea1bb2c69c4e9489aaf8ec827b2e3
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-03-23 11:57:46 +0900

    [D3] Title: N-Queen, Time: 140 ms, Memory: 21,132 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Solution {
+	// y좌표 일치?
+	static boolean[] visitY = new boolean[40];
+	// 오른쪽 위로 가는 방향의 대각
+	static boolean[] visitRightUp = new boolean[40];
+	// 오른쪽 아래로 가는 방향의 대각
+	static boolean[] visitRightDown = new boolean[40];
+	static int cnt, N, T = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			N = sc.nextInt();
+			cnt = 0;
+			backtracking(0);
+			System.out.printf("#%d ", tc);
+			System.out.println(cnt);
+		}
+	}
+	public static void backtracking(int num) {
+		// base case
+		if (num == N) {
+			cnt++;
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (visitY[i] || visitRightUp[i+num] || visitRightDown[num-i+N-1]) continue;
+			visitY[i] = true;
+			visitRightUp[i + num] = true;
+			visitRightDown[num-i+N-1] = true;
+			backtracking(num+1);
+			visitY[i] = false;
+			visitRightUp[i + num] = false;
+			visitRightDown[num-i+N-1] = false;
+		}
+	}
+}
