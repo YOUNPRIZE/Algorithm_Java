a30e5ffc494487369a48d1508cb77eeab94700b7
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-03-26 02:22:04 +0900

    [Silver IV] Title: 상근이의 여행, Time: 316 ms, Memory: 45860 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
 		for (int tc = 0; tc < T; tc++) {
-			int V = sc.nextInt();
-			int E = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
 			
 			for (int i = 0; i < E; i++) {
-				int A = sc.nextInt();
-				int B = sc.nextInt();
+				st = new StringTokenizer(br.readLine());
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
 			}
 			sb.append(V-1).append("\n");
 		}
