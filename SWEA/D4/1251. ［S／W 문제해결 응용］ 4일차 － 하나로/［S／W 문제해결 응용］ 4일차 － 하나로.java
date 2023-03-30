6ddd10a405728739346f2fc80ff62a191b5da34a
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-03-30 17:25:29 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 686 ms, Memory: 145,032 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Solution {
 	static int N;
 	static double E;
-	static int[] parents, coordX, coordY;
-	static double[][] graph;
+	static int[] coordX, coordY;
+	//static double[][] graph;
 	static long ans;
 	static ArrayList<Edge> list;
 	public static void main(String[] args) {
@@ -15,13 +16,11 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
-			list = new ArrayList<>();
 			sb.append("#").append(tc).append(" ");
 			N = sc.nextInt();
-			parents = new int[N+1];
 			coordX = new int[N+1];
 			coordY = new int[N+1];
-			graph = new double[N+1][N+1];
+			//graph = new double[N+1][N+1];
 			for (int i = 1; i < N+1; i++) {
 				coordX[i] = sc.nextInt();
 			}
@@ -29,6 +28,14 @@ public class Solution {
 				coordY[i] = sc.nextInt();
 			}
 			
+			List<Edge>[] adjList = new ArrayList[N+1];
+			
+			
+			
+			for(int i = 0 ; i<N+1; i++) {
+				adjList[i] = new ArrayList<>();
+			}
+//			System.out.println(Arrays.toString(adjList));
 			E = sc.nextDouble();
 			for (int i = 1; i <= N; i++) {
 				for (int j = 1; j <= N; j++) {
@@ -36,38 +43,63 @@ public class Solution {
 						double xSquare = Math.pow(Math.abs(coordX[i]-coordX[j]), 2);
 						double ySquare = Math.pow(Math.abs(coordY[i]-coordY[j]), 2);
 						double dist = xSquare + ySquare;
-//						double price = Math.pow(dist, 2);
-//						graph[i][j] = price;
-//						graph[j][i] = price;
-						graph[i][j] = dist;
-						graph[j][i] = dist;
+//						graph[i][j] = dist;
+//						graph[j][i] = dist;
+						adjList[i].add(new Edge(i,j,dist));
+						adjList[j].add(new Edge(j,i,dist));
 					}
 				}
 			}
-//			for (int i = 0; i < N; i++) {
-//				System.out.println(Arrays.toString(graph[i]));
+//			for (int i = 0; i < adjList.length; i++) {
+//				System.out.println(adjList[i]);
 //			}
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= N; j++) {
-					list.add(new Edge(i,j,graph[i][j]));
-				}
-			}
-			Collections.sort(list);
+			boolean[] visit = new boolean[N+1];
 			
-			make();
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
 			
-			int cnt = 0;
+			visit[1] = true;
+			
+			pq.addAll(adjList[1]);
+			
+			int pick = 1; 
 			ans = 0;
-			for (Edge e : list) {
-				if (unionSet(e.from, e.to)) {
-					cnt++;
-					ans += e.cost;
-					if (cnt == N - 1) break;
-				}
+			
+			while(pick != N) {
+				Edge e = pq.poll();
+				if (visit[e.to]) continue;
+				
+				ans += e.cost;
+				pq.addAll(adjList[e.to]);
+				visit[e.to] = true;
+				pick++;
 			}
-//			System.out.println(ans);
 			long res = Math.round(ans * E);
 			sb.append(res).append("\n");
+//			for (int i = 1; i <= N; i++) {
+//				for (int j = 1; j <= N; j++) {
+//					list.add(new Edge(i,j,graph[i][j]));
+//				}
+//			}
+//			// 비용순으로 정렬
+//			Collections.sort(list);
+//			
+//			make();
+//			
+//			int cnt = 0;
+//			ans = 0;
+//			for (Edge e : list) {
+//				// 이어져 있다면
+//				if (unionSet(e.from, e.to)) {
+//					// 간선 추가
+//					cnt++;
+//					// 비용 추가
+//					ans += e.cost;
+//					// 다 이어졌으면 for문 탈출
+//					if (cnt == N - 1) break;
+//				}
+//			}
+//			long res = Math.round(ans * E);
+//			sb.append(res).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
@@ -88,24 +120,5 @@ public class Solution {
 		public int compareTo(Edge o) {
 			return Double.compare(this.cost, o.cost);
 		}
-	}
-	private static void make() {
-		for (int i = 1; i <= N; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	private static int findSet(int v) {
-		if (parents[v] == v) return v;
-		return parents[v] = findSet(parents[v]);
-	}
-	
-	private static boolean unionSet(int from, int to) {
-		int fromRoot = findSet(from);
-		int toRoot = findSet(to);
-		
-		if (fromRoot == toRoot) return false;
-		parents[toRoot] = fromRoot;
-		return true;
 	}
 }
