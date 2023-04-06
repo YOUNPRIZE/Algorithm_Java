a53ba45aa14e856420284f62f9693df7c74ae661
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-06 11:40:10 +0900

    [Gold I] Title: 다리 만들기 2, Time: 80 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,23 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node {
-		int x; // from
-		int y; // to
-		int cnt;
-		public Node(int x, int y, int cnt) {
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-	}
 	static class Node2 implements Comparable<Node2> {
 		int x; // from
 		int y; // to
@@ -39,7 +28,7 @@ public class Main {
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	static int[] parent;
-	static Queue<Node> q;
+	static Queue<int[]> q;
 	static PriorityQueue<Node2> qConn;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -52,9 +41,7 @@ public class Main {
 		qConn = new PriorityQueue<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-			}
+			for (int j = 0; j < M; j++) graph[i][j] = Integer.parseInt(st.nextToken());
 		}
 		idx = 1;
 		for (int i = 0; i < N; i++) {
@@ -65,16 +52,14 @@ public class Main {
 				}
 			}
 		}
-//		for (int i = 0; i < N; i++) System.out.println(Arrays.toString(graph[i]));
 		visit = new boolean[N][M];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (graph[i][j] >= 1) {
-					connectIsland(i, j, graph[i][j]);
-				}
+				if (graph[i][j] >= 1) connectIsland(i, j, graph[i][j]);
 			}
 		}
 		
+		// 밑은 크루스칼 알고리즘 구현부
 		parent = new int[idx];
 		
 		for (int i = 1; i < idx; i++) {
@@ -83,34 +68,28 @@ public class Main {
 		
 		int len = 0;
 		int count = 0;
-//		if (qConn.size() == 0) {
-//			System.out.println(-1);
-//			return;
-//		}
 		while(!qConn.isEmpty()) {
 			Node2 node = qConn.poll();
 			int from = node.x;
 			int to = node.y;
 			int leng = node.cnt;
-//			System.out.println("from : " + from + ", to : " + to + ", leng : " + leng);
 			if (find(from) != find(to)) {
 				union(from, to);
 				len += leng;
 				count++;
 			}
 		}
-//		System.out.println(idx);
-//		System.out.println(count);
 		if (len == 0 || count != idx - 2) System.out.println(-1);
 		else System.out.println(len);
 	}
+	// 섬 구역 나누기
 	public static void divideIsland(int x, int y) {
-		q.add(new Node(x,y,0));
+		q.add(new int[] {x, y});
 		visit[x][y] = true;
 		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			int cx = cur.x;
-			int cy = cur.y;
+			int[] cur = q.poll();
+			int cx = cur[0];
+			int cy = cur[1];
 			graph[cx][cy] = idx;
 			for (int i = 0; i < 4; i++) {
 				int nx = cx + dx[i];
@@ -119,11 +98,12 @@ public class Main {
 				if (nx < 0 || ny < 0 || nx >= N || ny >= M || visit[nx][ny]) continue;
 				if (graph[nx][ny] == 1) {
 					visit[nx][ny] = true;
-					q.add(new Node(nx, ny, 0));
+					q.add(new int[] {nx, ny});
 				}
 			}
 		}
 	}
+	// 섬 연결 해주기
 	public static void connectIsland(int x, int y, int num) {
 		int cx = x;
 		int cy = y;
@@ -145,8 +125,6 @@ public class Main {
 					else {
 						// 1보다 크면 pq에 추가
 						if (length > 1) {
-//							System.out.println("!23412341234");
-//							System.out.println("from : " + num + ", to : " + graph[cx][cy] + " length : " + length);
 							qConn.add(new Node2(num, graph[cx][cy], length));
 						}
 						cx = x;
