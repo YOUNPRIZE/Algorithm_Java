71c0f0a310a0277940f6cac71bc868d81c2199db
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-12 09:55:01 +0900

    [Bronze V] Title: 개수 세기, Time: 120 ms, Memory: 12952 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int target = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			if (arr[i] == target) cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
