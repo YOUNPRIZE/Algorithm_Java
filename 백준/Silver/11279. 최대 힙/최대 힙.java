5242a6794e0478873650bcac8488b94b149b1c7e
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-17 22:19:57 +0900

    [Silver II] Title: 최대 힙, Time: 308 ms, Memory: 25572 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -7,14 +7,16 @@ import java.util.PriorityQueue;
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 		for (int i = 0; i < N; i++) {
 			int x = Integer.parseInt(br.readLine());
 			if (x == 0) {
-				if (pq.size() == 0) System.out.println(0);
-				else System.out.println(pq.poll());
+				if (pq.size() == 0) sb.append(0).append("\n");
+				else sb.append(pq.poll()).append("\n");
 			} else pq.add(x);
 		}
+		System.out.println(sb.toString());
 	}
 }
