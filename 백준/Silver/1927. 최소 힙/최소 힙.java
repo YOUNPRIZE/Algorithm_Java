617526879cfcf0647d46b411f863b9d091ef04eb
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-18 20:11:09 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 24580 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -5,16 +5,19 @@ import java.util.PriorityQueue;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 제일 작은 값을 poll 하는 우선순위큐 선언 및 초기화
 		PriorityQueue<Integer> q = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(br.readLine());
 			if (num == 0) {
-				if (q.size() == 0) System.out.println(0);
-				else System.out.println(q.poll());
+				if (q.size() == 0) sb.append(0).append("\n");
+				else sb.append(q.poll()).append("\n");
 			}
 			else q.add(num);
 		}
+		System.out.println(sb.toString());
 	}
 }
