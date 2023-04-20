b2b2fe1f255cbe7c0fe70f02cecd5892d67346a2
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-20 12:00:11 +0900

    [level 2] Title: 더 맵게, Time: 1611.03 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    public int solution(int[] scoville, int K) {
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+        int answer = 0;
+        boolean flag = false;
+        if (pq.peek() >= K) {
+            return 0;
+        } else {
+            while (pq.size() > 1) {
+                answer++;
+                int ans = 0;
+                ans += pq.poll();
+                ans += pq.poll() * 2;
+                pq.add(ans);
+                if (pq.peek() >= K) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) return answer;
+            else return -1;
+        } 
+    }
+}
