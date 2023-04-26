53e5135e501918c55a0ac02000a7991479e376ea
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-26 15:38:34 +0900

    [Silver II] Title: 가장 긴 부분 수열 구하기, Time: 476 ms, Memory: 120040 KB -BaekjoonHub

--- 백준/Silver/23630. 가장 긴 부분 수열 구하기/가장 긴 부분 수열 구하기.java
+++ 백준/Silver/23630. 가장 긴 부분 수열 구하기/가장 긴 부분 수열 구하기.java
@@ -1,33 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int[] sequence = new int[n];
-        for (int i = 0; i < n; i++) {
-            sequence[i] = scanner.nextInt();
-        }
-
-        int[] bitCount = new int[32];
-
-        for (int i = 0; i < n; i++) {
-            int temp = sequence[i];
-            int j = 0;
-            while (temp > 0) {
-                bitCount[j] += (temp % 2);
-                temp = temp / 2;
-                j++;
-            }
-        }
-
-        int maxBitCount = 0;
-        for (int i = 0; i < 32; i++) {
-            if (bitCount[i] > maxBitCount) {
-                maxBitCount = bitCount[i];
-            }
-        }
-        System.out.println(maxBitCount);
-    }
+	static int N, ans;
+	static int[] arr, bitCount;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		bitCount = new int[32];
+		
+		for (int i = 0; i < N; i++) {
+			int temp = arr[i];
+			int j = 0;
+			while (temp > 0) {
+				bitCount[j] += (temp % 2);
+				temp = temp / 2;
+				j++;
+			}
+		}
+		
+		int maxBitCount = 0;
+		
+		for (int i = 0; i < 32; i++) {
+			if (bitCount[i] > maxBitCount) maxBitCount = bitCount[i];
+		}
+		System.out.println(maxBitCount);
+	}
 }
