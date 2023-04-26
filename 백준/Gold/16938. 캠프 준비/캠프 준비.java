2ee820837bcb6825e3242bcdf5e224488e762dba
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-04-26 14:32:31 +0900

    [Gold V] Title: 캠프 준비, Time: 96 ms, Memory: 11720 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -11,6 +11,7 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
+//        System.out.println((1 << n));
         l = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
@@ -29,17 +30,22 @@ public class Main {
     
     public static void bitmasking() {
     	for (int i = 1; i < (1 << n); i++) {
+    		// 1의 개수를 세기 위해 Integer.bitCount() 메소드 사용
             if (Integer.bitCount(i) < 2) { // 문제를 2개 이상 풀어야 함
                 continue;
             }
+            // 선택한 문제의 난이도의 합
             int t = 0;
-            int m = 0;
+            // 선택한 문제를 이진수로 표현한 것을 합친 값
+//            int m = 0;
+            // 최솟값
             int mn = Integer.MAX_VALUE;
+            // 최댓값
             int mx = Integer.MIN_VALUE;
             for (int j = 0; j < n; j++) {
                 if ((i & (1 << j)) != 0) {
                     t += a[j];
-                    m |= 1 << j;
+//                    m |= 1 << j;
                     mn = Math.min(mn, a[j]);
                     mx = Math.max(mx, a[j]);
                 }
