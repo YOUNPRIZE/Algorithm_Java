e2b30fae11b25ccf022039760ad62969956e3cce
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-05-11 17:31:33 +0900

    [Bronze III] Title: 세 수, Time: 112 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			int num = sc.nextInt();
+			arr[i] = num;
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[1]);
+	}
+}
