56f160c48ae4502243b046cb67992ad32dd9988d
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-07-11 01:23:04 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 252 ms, Memory: 22052 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -4,34 +4,28 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        int[] heights = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            heights[i] = Integer.parseInt(br.readLine());
-        }
-        
-        long totalViewCount = calculateTotalViewCount(heights);
-        
-        System.out.println(totalViewCount);
-    }
-    
-    public static long calculateTotalViewCount(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
-        long totalViewCount = 0;
-        
-        for (int i = 0; i < heights.length; i++) {
-            while (!stack.isEmpty() && heights[stack.peek()] <= heights[i]) {
-                stack.pop();
-            }
-            
-            totalViewCount += stack.size();
-            stack.push(i);
-        }
-        
-        return totalViewCount;
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		long ans = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			while(!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+				stack.pop();
+			}
+			ans += stack.size();
+			stack.push(i);
+		}
+		
+		System.out.println(ans);
+	}
 }
