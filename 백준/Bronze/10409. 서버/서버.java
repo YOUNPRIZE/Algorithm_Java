357794f0cb998343f831a450df6afbd802a6873c
YOUN <76830587+YOUNPRIZE@users.noreply.github.com>
2023-08-13 12:04:15 +0900

    [Bronze III] Title: 서버, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/10409. 서버/서버.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		int i = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(i = 0; i < n; i++) {
+			T -= Integer.parseInt(st.nextToken());
+			if(T < 0) {
+				break;
+			}
+		}
+		System.out.println(i);
+	}
+
+}
