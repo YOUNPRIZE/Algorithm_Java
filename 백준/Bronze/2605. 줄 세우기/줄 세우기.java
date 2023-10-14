66b1472f9087a9ec365de157d75880dbec801f8e
YOUNPRIZE <76830587+YOUNPRIZE@users.noreply.github.com>
2023-10-14 23:16:45 +0900

    [Bronze II] Title: 줄 세우기, Time: 116 ms, Memory: 12960 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> students = new ArrayList<>();
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            students.add(sc.nextInt(), i); 
+        }
+        for (int i = N - 1; i >= 0; i--) { 
+            System.out.print(students.get(i) + " ");
+        }
+    }
+}
